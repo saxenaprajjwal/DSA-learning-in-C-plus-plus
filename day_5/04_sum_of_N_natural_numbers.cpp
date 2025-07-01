79bed5c7a32c5b49254866f70d0926fe2d074706
Prajjwal Saxena <prajjwalsaxena18@gmail.com>
2025-07-01 12:09:15 +0530

    inittial commit

--- day_5/04_sum_of_N_natural_numbers.cpp
+++ day_5/04_sum_of_N_natural_numbers.cpp
@@ -2,7 +2,17 @@
  using namespace std;
  int main(){
 
-   
+    int num;
+    cout << "enter your number = ";
+    cin >> num;                                                           
+    int sum = 0;
+    
+    for(int i = 1; i <= num; i++ ){
+         sum = sum + i;
+    }
+    cout << "sum = " << sum <<  endl;
 
     return 0 ;
  }
+
+
