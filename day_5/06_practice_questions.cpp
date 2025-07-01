58212f139df8eb83493320d9a04868ff2cc9b042
Prajjwal Saxena <prajjwalsaxena18@gmail.com>
2025-07-01 15:09:46 +0530

    initial commit

--- day_5/06_practice_questions.cpp
+++ day_5/06_practice_questions.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
  using namespace std;
- int main(){
-    int i = 10;
-    while(i <= 25) {
-        cout << i << " " ;
-        i++;
+ int main(){ 
+    
+    // print the sqaure patetrn
+
+    for(int i =1;i <=4;i++ ){
+        cout << "****" << endl;
     }
     
-
+    // PRINT numbers from n to 1 using for loop 
+    int n;
+    cout << "enter n = ";
+    cin >> n;
+    for(int i = n; i >=1 ; i--){
+        cout << i << " ";
+}
+    cout << endl;
     return 0 ;
  }
