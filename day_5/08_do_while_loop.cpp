1fa66f1df5e483bc5067c9f380a285b8557b62af
saxenaprajjwal <prajjwalsaxena18@gmail.com>
2025-07-04 12:22:02 +0530

    initial commit

--- day_5/08_do_while_loop.cpp
+++ day_5/08_do_while_loop.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
     int main(){
 
+// work gets done at least once irrespective of the condition 
 
+        int i = 1;
+        do {
+            cout << i << " ";
+            i++;
+        } while(i<=5);
+        cout << endl;
 
+// diff btw while and do while loop 
 
+         int val = 1;
+        do{
+            cout << "prajjwal saxena in do-while loop" << endl;
+        }while(val > 5);
 
+        while( val > 5){
+            cout << "prajjwal saxena in while loop";  
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
         return 0;
     }
