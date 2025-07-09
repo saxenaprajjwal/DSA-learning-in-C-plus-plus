d3faf1e10af1df76eb9d33af67a3db16ae38c4db
Prajjwal Saxena <prajjwalsaxena18@gmail.com>
2025-07-09 11:19:54 +0530

    prime numbers code

--- day_5/13_check_for_prime.cpp
+++ day_5/13_check_for_prime.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 
 int main(){
-   
-    
+
+    int n = 191;
+    bool isPrime = true;
+
+    for(int x = 2; x <= n-1; x++){
+        if ( n % x == 0){
+            isPrime = false;
+            break;
+        }
+    }
+    if(isPrime == true){                                  // can only write isPrime no need to write  = true 
+        cout << "number is prime" << endl;
+    }else{
+        cout << "number is composite" << endl;
+    }
+
     return 0;
 }
