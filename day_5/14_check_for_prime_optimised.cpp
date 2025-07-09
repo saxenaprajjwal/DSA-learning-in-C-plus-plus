98ddcbff0d9e7e02211b535dcdedc40a224e2f44
Prajjwal Saxena <prajjwalsaxena18@gmail.com>
2025-07-09 11:26:31 +0530

    basic code for prime numbers

---
+++ day_5/14_check_for_prime_optimised.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>         // to calculate sqaure root and there many more math functions in this header file
+using namespace std;
+
+int main(){
+
+//optimised code for prime no checking 
+
+    int n = 191;
+    bool isPrime = true;
+
+    for(int x = 2; x <= sqrt(n); x++){                    // code runs from 2 to sqare root of n instead of n-1
+        if ( n % x == 0){
+            isPrime = false;
+            break;
+        }
+    }
+    if(isPrime == true){                                  // can only write isPrime no need to write  = true 
+        cout << "number is prime" << endl;
+    }else{
+        cout << "number is composite" << endl;
+    }
+
+    return 0;
+}
