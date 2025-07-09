1e364d8afc728bfb0f7a34faf4296a8399323fe5
Prajjwal Saxena <prajjwalsaxena18@gmail.com>
2025-07-09 11:58:01 +0530

    practice questions

---
+++ day_5/15_loops_questions.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>         
+using namespace std;
+
+int main(){
+
+// (Assignment Questions)
+// Question 1 : WAP to find the Factorial of a number entered by the user.
+// Hint : factorial of a number (n) = n * (n-1) * (n-2) * (n-3) * ........... * 1 and exists for positive numbers only. We write factorial as n!
+// So, factorial of 0! = 1, 1! = 1, 2! = 2, 3! = 6, 4! = 24 and so on.
+// Note - Please do not confuse factorial with NOT EQUAL TO operator, they are not the same.
+
+
+
+
+
+// Question 2 : WAP to print the multiplication table of a number, entered by the user.
+
+
+
+
+
+
+// Question 3 : WAP to input a number and check whether the number is an Armstrong
+// number or not.
+// An Armstrong number is a number that is equal to the sum of cubes of its digits.
+
+
+
+
+
+// Question 4 : For a positive N , WAP that prints all the prime numbers from 2 to N. (Assume N >= 2)
+
+
+
+// Question 5 : For a positive N , WAP that prints the first N Fibonacci numbers. (Assume N >= 2)
+// Fibonacci series : 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ….
+// This is a series where each number is a sum of previous 2 numbers in the series. Eg : 1 = 0 + 1,
+// 2 = 1 + 1,
+// 3 = 1 + 2,
+// 5 = 2 + 3,
+// 8 = 3 + 5 & so on.
+
+
+    
+
+
+
+
+
+    return 0;
+}
