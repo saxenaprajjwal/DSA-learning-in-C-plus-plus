97acac9162f5efa3fc9f81968047fe2ba2e29c02
Prajjwal Saxena <prajjwalsaxena18@gmail.com>
2025-07-16 12:08:11 +0530

    nested loops

--- day_6_Patterns/01_nested_loops.cpp
+++ day_6_Patterns/01_nested_loops.cpp
@@ -3,18 +3,17 @@ using namespace std;
 
 int main(){
 
-    int i,j; 
     int n = 5;                  
-
+    
     // outer loop 
-    for(i=1;i<=n;i++){
+    for(int i=1;i<=n;i++){
+        int val = i;
         // inner loop
-        for(j=1;j<=n;j++){
+        for(int j=1;j<=n;j++){
             //work
-            cout << i << " ";
+            cout << val << " ";
         }
         cout << endl;
     }
-
     return 0;
 }
