a857e6a6d657f8e078d47a45045b5f6f4584a549
Prajjwal Saxena <prajjwalsaxena18@gmail.com>
2025-07-19 12:39:51 +0530

    declaration

---
+++ day_7_Functions/02_forward_declarion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+// Declaration: the function name, return type,and parametres(if any)
+
+void sayHello(); //function declare
+
+int main() {
+    sayHello(); 
+    return 0;
+}
+
+void sayHello(){
+    cout << "Hello\n";
+}
