1cbf06553439116c069983d64dc4b8ea0954bb3c
Prajjwal Saxena <prajjwalsaxena18@gmail.com>
2025-07-23 18:47:38 +0530

    factorial function

--- day_7_Functions/05_factorial.cpp
+++ day_7_Functions/05_factorial.cpp
@@ -3,9 +3,23 @@ using namespace std;
 
 
 
+int factorial(int n){
+    int fact = 1;
+    for(int i=1;i<=n;i++){
+        fact = fact * i;
+    }
+    cout << "factorial(" << n << ") = " << fact << endl;
+    return fact;
+}
 
 int main() {
+    factorial(0);
+    factorial(1);
+    factorial(2);
+    factorial(3);
+    factorial(4);
+    factorial(5);
+    factorial(6);
 
-
-    return 0;
+return 0;
 }
