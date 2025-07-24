c5be88f84a2eba67c7a482b5eae2ab922392ee63
Prajjwal Saxena <prajjwalsaxena18@gmail.com>
2025-07-24 21:31:11 +0530

    optimesed prime no check

--- day_7_Functions/08_practice_question.cpp
+++ day_7_Functions/08_practice_question.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // write a function to check if a number is prime or not
-
+// 1st method
 bool isPrime(int n){
     if (n == 1){
         return false;
@@ -16,7 +16,20 @@ for(int i=2; i<=2; i++){
     return true;
 }
 
+//2nd method(optimsed)
+bool isPrime2(int n){
+    if( n == 1){
+        return false;
+    }
+    for(int i=2; i*i<=n; i++){
+        if (n % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
 int main() {
     cout << isPrime(23) << endl;
+    cout << isPrime2(23) << endl;
      return 0;
 }
