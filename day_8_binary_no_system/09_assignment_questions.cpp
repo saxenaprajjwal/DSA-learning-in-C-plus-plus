f862b46a2973231c2d6f94efd468df60ec8ae3d6
Prajjwal Saxena <prajjwalsaxena18@gmail.com>
2025-07-28 14:47:58 +0530

    assignment

---
+++ day_8_binary_no_system/09_assignment_questions.cpp
@@ -0,0 +1,36 @@
+// Binary Number System
+// (Assignment Questions)
+// *Try to use the proper as well as the shortcut method of powers of 2, as taught in the lecture.
+// Question 1 : Convert the following binary numbers into decimal forms :
+// ●
+// 111111
+// ●
+// 10110
+// ●
+// 10011
+// ●
+// 110010/
+
+
+// Question 2 : Convert the following decimal numbers into binary forms :
+// ●
+// 25
+// ●
+// 49
+// ●
+// 31
+// ●
+// 88
+
+
+// Question 3 : Following are the rules of adding 2 binary digits :
+// 0 + 0 = 0, carry = 0
+// 1 + 0 = 1, carry = 0
+// 0 + 1 = 1, carry = 0
+// 1 + 1 = 0, carry = 1
+// So, in math if 2 + 3 = 5, in binary it looks like 1 0
+// + 1 1
+// 1 0 1
+
+// Using this method, try to add these 2 numbers (63 & 22) in their binary form and verify that the binary output is equal to the decimal value 85.
+// Bonus : Try to read up about 3 Bitwise Operators in C++: OR (|), AND (&) and NOT (~). [Refer Link]
