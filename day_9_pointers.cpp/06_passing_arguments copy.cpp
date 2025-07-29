03132482ec2a971563cd70f51f88734e83cc4e7f
Prajjwal Saxena <prajjwalsaxena18@gmail.com>
2025-07-29 18:46:39 +0530

    Implement pass by reference example using reference variables

--- day_9_pointers.cpp/06_passing_arguments copy.cpp
+++ day_9_pointers.cpp/06_passing_arguments copy.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// PASS BY VALUE (when parameter is a copy of actual argument varaible in memory) 
-// void changeA(int a){
-//         a = 20;
-//         cout << a << "\n"; 
-//     }
-
 // PASS BY REFERENCE (when we pass the reference of argument to the fuction)
 
 //METHOD 1 -- using pointer
@@ -17,7 +11,6 @@ void changeA(int *ptr){
 
 int main(){
     int a = 10;
-    // changeA(a);          // do not chnages the value of variable in function in main function
     changeA(&a);           // chnages the value of variable in function in main function
     cout << a << "\n";
 
