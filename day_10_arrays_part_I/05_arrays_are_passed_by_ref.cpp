6b8dd5c04ba315ec61a206e43efc5f7e1c18e022
Prajjwal Saxena <prajjwalsaxena18@gmail.com>
2025-07-30 15:02:11 +0530

    Add example demonstrating pointer usage with a variable

--- day_10_arrays_part_I/05_arrays_are_passed_by_ref.cpp
+++ day_10_arrays_part_I/05_arrays_are_passed_by_ref.cpp
@@ -4,6 +4,16 @@ using namespace std;
 // Array name can be converted to a pointer 
 
 int main(){
+    int a = 5;
+    int *ptr = &a;
+    cout << ptr << endl; //0x...
+
+    // int arr[] = {5, 4, 3, 9, 12};
+    // int n = sizeof(arr) / sizeof(int);
+    // cout << arr << endl;
+    // cout << *arr << endl; //arr[0]
+    // cout << *(arr+1) << endl; //arr[1]
+    // cout << *(arr+2) << endl; //arr[2]
 
 
   
