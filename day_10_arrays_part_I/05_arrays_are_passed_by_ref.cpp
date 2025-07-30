6a1ab72f6ae41fa5535efc0d089d57c34d0e7dfb
Prajjwal Saxena <prajjwalsaxena18@gmail.com>
2025-07-30 17:44:27 +0530

    Refactor array demonstration to clarify pointer conversion and update function usage

--- day_10_arrays_part_I/05_arrays_are_passed_by_ref.cpp
+++ day_10_arrays_part_I/05_arrays_are_passed_by_ref.cpp
@@ -3,21 +3,25 @@ using namespace std;
 
 // Array name can be converted to a pointer 
 
+void (func(int arr[])) {
+    arr[0] = 1000;
+}
+
 int main(){
     int a = 5;
     int *ptr = &a;
     cout << ptr << endl; //0x...
 
-    // int arr[] = {5, 4, 3, 9, 12};
-    // int n = sizeof(arr) / sizeof(int);
-    // cout << arr << endl;
-    // cout << *arr << endl; //arr[0]
-    // cout << *(arr+1) << endl; //arr[1]
-    // cout << *(arr+2) << endl; //arr[2]
+    int arr[] = {1,2,3,4,5};
+    int n = sizeof(arr) / sizeof(int);
+    // KEY CONCEPT: Array name 'arr' is automatically converted to a pointer
+    // that points to the first element (arr[0])
+    // cout << arr << endl;  
+    // cout << *arr << endl;        // Same as arr[0] - prints 1
+    // cout << *(arr+1) << endl;    // Same as arr[1] - prints 2  
+    // cout << *(arr+2) << endl;    // Same as arr[2] - prints 3
 
-
-  
-    
-
+    func(arr);
+    cout << arr[0] << endl;
     return 0;
 }
