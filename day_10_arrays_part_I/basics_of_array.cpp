fd66d152e4ca036ee1df76cabfbf9fed8024fc3b
Prajjwal Saxena <prajjwalsaxena18@gmail.com>
2025-07-30 11:29:29 +0530

    Add initial implementation of basics_of_array

---
+++ day_10_arrays_part_I/basics_of_array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    // ARRAYs --
+    // linear collection of same type of elements that are stored together in contiguous memory spaces.
+    // in C++ index of an array starts from 0 
+
+    return 0;
+}
