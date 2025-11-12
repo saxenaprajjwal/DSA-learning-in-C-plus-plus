922462e71b401dc57845772bab9041628c576a2e
saxenaprajjwal <prajjwalsaxena18@gmail.com>
2025-11-12 13:21:24 +0530

    Best Time to Buy and Sell Stock problem

---
+++ day_11_arrays_part_II/06_buy_n_sell_stocks.cpp
@@ -0,0 +1,28 @@
+// Leetcode problem 121 - Best Time to Buy Sell and Stock (Level Easy)
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+void maxProfit(int *prices, int n){
+    int bestBuy[100000];
+    bestBuy[0] = INT_MAX;
+    for(int i=1; i<n; i++){
+        bestBuy[i] = min(bestBuy[i-1], prices[i-1]);
+    }
+    int maxProfit = 0;
+    for(int i=0; i<n; i++){
+        int currProfit = prices[i] - bestBuy[i];
+        maxProfit = max(currProfit, maxProfit);
+    }
+    // O(n+n) =  O(2n) =  O(n)
+    cout << maxProfit; 
+}
+
+
+int main(){
+    int prices[6] = {7,1,5,3,6,4};
+    int n = sizeof(prices) / sizeof(int);
+    maxProfit(prices, n);
+    return 0;
+} 
