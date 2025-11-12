13fdbd4c9a512ab7ecbc219020b099cc54ff515c
saxenaprajjwal <prajjwalsaxena18@gmail.com>
2025-11-12 13:26:39 +0530

    day 12 completed

---
+++ day_11_arrays_part_II/08_trapping_rainwater.cpp
@@ -0,0 +1,36 @@
+// Leetcode problem 42 - Tapping Rainwater (Level Hard)
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+void trap(int *heights,int n){
+    int leftMax[20000], rightMax[20000];
+    // can be initilised with int_min but there will be extra if condition we need to write 
+    leftMax[0] = heights[0];  
+    rightMax[n-1] = heights[n-1];
+
+    for(int i=1; i<n; i++){
+        leftMax[i] = max(leftMax[i-1], heights[i-1]);
+    }
+    for(int i=n-2; i>=0; i--){
+        rightMax[i] = max(rightMax[i+1], heights[i+1]);
+    }
+    int  waterTrapped = 0;
+    for(int i=0; i<n; i++) {
+        int currWater = min(leftMax[i], rightMax[i]) - heights[i];
+        if (currWater > 0){
+
+            waterTrapped += currWater;
+        }
+    }
+    cout << "waterTrapped = " << waterTrapped;}
+ 
+
+int main(){
+    int heights[7] = {4,2,0,6,3,2,5};
+    int n =  sizeof(heights)/sizeof(int);
+
+    trap(heights,n);
+    return 0;
+}
