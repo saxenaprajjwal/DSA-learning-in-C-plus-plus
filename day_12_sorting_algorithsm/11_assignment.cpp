3a731de9a274d43a5a126a3186776f86569427fa
saxenaprajjwal <prajjwalsaxena18@gmail.com>
2025-11-19 16:38:58 +0530

    assignment question

---
+++ day_12_sorting_algorithsm/11_assignment.cpp
@@ -0,0 +1,9 @@
+// Basic Sorting Algorithms
+// (Assignment Questions)
+// Question 1 : Use the following sorting algorithms to sort an array in DESCENDING
+// order :
+// a. Bubble Sort
+// b. Selection Sort
+// c. Insertion Sort
+// d. Counting Sort
+// You can use this array as an example : [3, 6, 2, 1, 8, 7, 4, 5, 3, 1]
